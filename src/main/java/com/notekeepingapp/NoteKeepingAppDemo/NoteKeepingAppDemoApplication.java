763d6799e31674e4c9cf7cd35d955d97426e8064
ganjali <gaddam.anjali@oracle.com>
2019-05-06 19:09:57 +0530

    Intialize a simple spring boot application

---
+++ src/main/java/com/notekeepingapp/NoteKeepingAppDemo/NoteKeepingAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.notekeepingapp.NoteKeepingAppDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteKeepingAppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteKeepingAppDemoApplication.class, args);
+    }
+
+}
