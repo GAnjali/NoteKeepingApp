a653c3869f107c9fb35ec8d7460ad0eb83e8936c
ganjali <gaddam.anjali@oracle.com>
2019-05-16 08:18:02 +0530

    adds Application SpringBootApplication class

--- src/main/java/com/notekeepingapp/NoteKeepingAppDemo/NoteKeepingAppDemoApplication.java
+++ src/main/java/com/notekeepingapp/NoteKeepingAppDemo/NoteKeepingAppDemoApplication.java
@@ -1,13 +1,12 @@
 package com.notekeepingapp.NoteKeepingAppDemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NoteKeepingAppDemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NoteKeepingAppDemoApplication.class, args);
     }
-
 }
