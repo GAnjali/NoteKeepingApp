b76facc983014d8ce44ab1135ab4a95b89224a14
ganjali <gaddam.anjali@oracle.com>
2019-05-19 23:12:33 +0530

    Deleted Note.js

--- src/main/resources/static/js/Notes.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    console.log("in notes from notes.js");
-    $.ajax({
-        url: "/list-notes/Anju", success: function (result) {
-            console.log(result);
-        }
-    });
-});
